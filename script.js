0870c6a339393dc6deb00e114968b5b242002cb7
Jacqueline Williamson <jacqueline_25@hotmail.com>
2020-08-23 18:26:51 -0400

    number 4...why can't I get this

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Add validation!');
+
+
+const form = document.querySelector("#parking-form")
+
+form.addEventListener('submit', function(event){
+    event.preventDefault()
+    const total = document.querySelector("#total").value
+    const days = document.querySelector("#days").value
+
+    const priceList = document.querySelector("#priceList")
+    const priceTotal = total * days 
+    priceList.innerHTML += priceTotal
+})
+
+console.log(priceTotal);
