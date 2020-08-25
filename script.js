5f16472509c22c86265de1701f751c34db48e837
Jacqueline Williamson <jacqueline_25@hotmail.com>
2020-08-25 09:18:47 -0400

    overthinking number 5

--- script.js
+++ script.js
@@ -8,8 +8,41 @@ form.addEventListener('submit', function(event){
     const tripCost = document.querySelector("#total");
     const cost = (days * 5);
     tripCost.innerText = "$" + cost ;
+
+    let pricing = []
+    const startDateString = document.querySelector("#start-date").value
+    const startDate = new Date (startDateString)
+    startDate
+    
+    for (const i = 0; i < days; i++) {
+       
+    }
+
 })
     
+let daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+let weekends = ["Sunday", "Saturday"]
+let pricing = []
+for (let day of daysOfWeek){
+    if (day === daysOfWeek[1,2,3,4,5]){
+        tripCost = (days * 5) 
+    } else if (day === daysOfWeek[0,6]){
+        tripCost = (days *7)
+    } return
+
+    //This seemed workable after getting the date to help us determine days of week, which I haven't worked out yet,
+    //but you would also want to add both 'tripCost' values, in which case they cannot be the same constant name?
+    
+}
+//Find the date; 
+//Determine the day of the week for the date; 
+//determine cost for that day, then each day following based on $5 or $7 price;
+//How many weekdays = x    How many weekend days = y
+//Calculate the cost of the amount of days [($5 * x) + ($7 * y)] = TotalCost
+
+
+
 
 //The requirements have changed for calculating cost. The new cost is $5 per weekday, 
 //and $7 per weekend day. `.map` and `.reduce` will be very helpful in calculating 
@@ -40,3 +73,13 @@ function luhnCheck(val) {
     }
     return (sum % 10) == 0;
 }
+
+const ccNumber = document.querySelector("#credit-card").value
+const ccValue = document.querySelector("#credit-card")
+    ccValue.addEventListener (function)
+    if (ccValue.length !== 16) {
+        ccValue.setCustomValidity("Credit card must be 16 digits")
+    }
+    else 
+        ccValue.setCustomValidity("")
+    }
